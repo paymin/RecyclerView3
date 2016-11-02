b40af7bf5040bf32c69893b9aca9d89cf9aa5cac
Irsyad Rahman <irsyadrahman78@gmail.com>
2016-11-02 14:23:25 +0700

    RecyclerView View Detail

--- app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/adapter/HotelAdapter.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/adapter/HotelAdapter.java
@@ -1,5 +1,7 @@
 package id.sch.smktelkom_mlg.learn.recyclerview3.adapter;
 
+import android.content.Context;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,10 +20,13 @@ import id.sch.smktelkom_mlg.learn.recyclerview3.model.Hotel;
 
 public class HotelAdapter extends RecyclerView.Adapter<HotelAdapter.ViewHolder> {
 
+    IHotelAdapter mIHotelAdapter;
+
     ArrayList<Hotel> hotelList;
 
-    public HotelAdapter(ArrayList<Hotel> hotelList) {
+    public HotelAdapter(Context context, ArrayList<Hotel> hotelList) {
         this.hotelList = hotelList;
+        mIHotelAdapter = (IHotelAdapter) context;
     }
 
     @Override
@@ -36,7 +41,7 @@ public class HotelAdapter extends RecyclerView.Adapter<HotelAdapter.ViewHolder> 
         Hotel hotel = hotelList.get(position);
         holder.tvJudul.setText(hotel.judul);
         holder.tvDeskripsi.setText(hotel.deskripsi);
-        holder.ivFoto.setImageDrawable(hotel.foto);
+        holder.ivFoto.setImageURI(Uri.parse(String.valueOf(hotel.foto)));
     }
 
     @Override
@@ -51,11 +56,27 @@ public class HotelAdapter extends RecyclerView.Adapter<HotelAdapter.ViewHolder> 
         TextView tvJudul;
         TextView tvDeskripsi;
 
+
+
         public ViewHolder(View itemView) {
             super(itemView);
             ivFoto = (ImageView) itemView.findViewById(R.id.imageView);
             tvJudul = (TextView) itemView.findViewById(R.id.textViewJudul);
             tvDeskripsi = (TextView) itemView.findViewById(R.id.textViewDeskripsi);
+
+            itemView.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v){
+                    mIHotelAdapter.doClick(getAdapterPosition());
+                }
+            });
         }
+
     }
+
+    public interface IHotelAdapter{
+        void doClick(int pos);
+    }
+
+
 }
--- app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/model/Hotel.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/model/Hotel.java
@@ -2,18 +2,25 @@ package id.sch.smktelkom_mlg.learn.recyclerview3.model;
 
 import android.graphics.drawable.Drawable;
 
+import java.io.Serializable;
+
 /**
  * Created by paymin on 01/11/2016.
  */
 
-public class Hotel {
+public class Hotel implements Serializable {
     public String judul;
     public String deskripsi;
-    public Drawable foto;
+    public String foto;
+    public String detail;
+    public String lokasi;
 
-    public Hotel(String judul, String deskripsi, Drawable foto){
+    public Hotel(String judul, String deskripsi, String foto, String detail, String lokasi){
         this.judul = judul;
         this.deskripsi = deskripsi;
         this.foto = foto;
+        this.detail = detail;
+        this.lokasi = lokasi;
     }
+
 }
