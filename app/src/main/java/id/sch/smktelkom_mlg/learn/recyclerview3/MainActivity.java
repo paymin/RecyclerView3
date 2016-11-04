e1638a582da0bef2f979ab9f008b4b44abd7e508
Irsyad Rahman <irsyadrahman78@gmail.com>
2016-11-04 09:30:55 +0700

    RecyclerView Add

--- app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/MainActivity.java
@@ -24,6 +24,7 @@ import id.sch.smktelkom_mlg.learn.recyclerview3.model.Hotel;
 
 public class MainActivity extends AppCompatActivity implements HotelAdapter.IHotelAdapter {
     public static final String HOTEL = "hotel";
+    public static final int REQUEST_CODE_ADD = 88;
 
     ArrayList<Hotel> mList = new ArrayList<>();
     HotelAdapter mAdapter;
@@ -47,10 +48,23 @@ public class MainActivity extends AppCompatActivity implements HotelAdapter.IHot
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                goAdd();
             }
         });
+    }
+
+    private void goAdd() {
+        startActivityForResult(new Intent(this,InputActivity.class), REQUEST_CODE_ADD);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_CODE_ADD && resultCode == RESULT_OK){
+            Hotel hotel = (Hotel) data.getSerializableExtra(HOTEL);
+            mList.add(hotel);
+            mAdapter.notifyDataSetChanged();
+        }
     }
 
     private void fillData() {
@@ -72,8 +86,8 @@ public class MainActivity extends AppCompatActivity implements HotelAdapter.IHot
         a.recycle();
 
         for (int i = 0; i < arJudul.length; i++) {
-            mList.add(new Hotel(arJudul[i], arDeskripsi[i],
-                    arFoto[i], arDetail[i], arLokasi[i]));
+            mList.add(new Hotel(arJudul[i], arDeskripsi[i], arDetail[i],
+                    arLokasi[i],arFoto[i]));
         }
         mAdapter.notifyDataSetChanged();
     }
--- app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/model/Hotel.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/model/Hotel.java
@@ -11,16 +11,16 @@ import java.io.Serializable;
 public class Hotel implements Serializable {
     public String judul;
     public String deskripsi;
-    public String foto;
     public String detail;
+    public String foto;
     public String lokasi;
 
-    public Hotel(String judul, String deskripsi, String foto, String detail, String lokasi){
+    public Hotel(String judul, String deskripsi, String detail, String lokasi, String foto){
         this.judul = judul;
         this.deskripsi = deskripsi;
-        this.foto = foto;
         this.detail = detail;
         this.lokasi = lokasi;
+        this.foto = foto;
     }
 
 }
